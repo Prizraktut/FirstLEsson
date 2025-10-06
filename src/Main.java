fbb43f4d075163d4c961df55f2578bd9af8f3063
Evgeny Kurbatov <eugkur@mail.ru>
2025-10-06 18:30:04 +0300

    FirstLEsson

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         long car2 = 12343563;
         long car3 = car*car2;
         System.out.println(car3);
+        System.out.println(car);
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
